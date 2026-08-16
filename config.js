741aaf18a9a49d693ed715ecd5e23d965079ec46
rs9409035-maker <rs9409035@gmail.com>
2026-08-16 12:19:07 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// ============================================
+// CENTRAL CONFIG - update this one line only
+// ============================================
+const API_URL = "https://script.google.com/macros/s/AKfycbwcza4hvavPQQ7LxVGevM0FWCGM7PpQAEUNrdc7wlTsP5hxhSyN53MKMSloXYU0HT-BVw/exec";
